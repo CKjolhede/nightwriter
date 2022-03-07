e4b67dd9bbba0eb37d2e70bef25f6fb8e5f5579b
Chris Kjolhede <ckjolhede@gmail.com>
2022-03-06 22:48:07 -0700

    Add files via upload

---
+++ night_writer.rb
@@ -0,0 +1,13 @@
+handle = File.open(ARGV[0], "r")
+
+incoming_text = handle.read
+
+handle.close
+
+puts "This is the incoming text #{incoming_text}"
+
+writer = File.open(ARGV[1], "w")
+write.write(braille.convert(incoming_text))
+write.close
+
+puts "Created #{ARGV[1]} containing #{incoming.text.length} characters"
---
+++ spec_helper.rb
@@ -0,0 +1,5 @@
+require 'simplecov'
+SimpleCov.start
+
+require relative "converter_braille.rb"
+require relative "converter_english.rb"
